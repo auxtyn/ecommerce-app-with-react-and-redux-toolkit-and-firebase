a79fb19a9d96605fc9ce0ff05e6b264613c748ef
auxtyn <austinserver042@gmail.com>
2023-12-29 09:39:35 +0100

    FIrst commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React  from 'react';
 
-function App() {
+import Nav from './components/Nav'
+import Home from './components/Home'
+import { Routes, Route } from 'react-router-dom'
+import About from './components/About'
+import Single from './components/Single'
+import Footer from './components/Footer'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="text-center">
+        <Nav />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Home />} />
+          <Route path="/single/:id" element={<Single />} />
+        </Routes>
+        <Footer/>
+      </div>
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
